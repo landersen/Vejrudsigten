4d044da2495c30009d8ef2819e81af07cb1abc01
Lasse Maglebjerg <lasse.maglebjerg@ist.com>
2021-10-22 08:45:56 +0200

    Added more handling regarding temperature and more headlines as text

--- TestWeatherForecast/WeatherForecastTest.cs
+++ TestWeatherForecast/WeatherForecastTest.cs
@@ -8,13 +8,13 @@ namespace TestWeatherForecast
     public class WeatherForecastTest
     {
         [Theory]
-        [InlineData("Kolding", "Regn", 13.6, "Andet", 12.1, "Vejret i Kolding er Regn og der er 13,6 grader. I går var det Andet og 12,1 grader")]
-        [InlineData("Nuuk", "Klart vejr", 1.3, "Klart vejr", 3.2, "Klart vejr igen i Nuuk med 1,3 grader idag og i går 3,2 grader")]
+        [InlineData("Kolding", "Sne", -6, "Regn", 3.6, "Det bliver koldt med -6 grader og der kommer Sne vejr i Kolding. I går var det Regn og 3,6 grader")]
+        [InlineData("Nuuk", "Regn", 1.3, "Skyet", 3.2, "Nu kommer der Regn i Nuuk med 1,3 grader idag og i går var det Skyet med 3,2 grader")]
+        [InlineData("Stockholm", "Skyet", 11, "Klart vejr", 1.2, "Skyet vejr i Stockholm med 11 grader. I går var det Klart vejr og 1,2 grader")]
         [InlineData("Oslo", "Skyet", 11, "Sne", -1.2, "Slut med Sne vejr og -1,2 grader. Nu bliver det Skyet og 11 grader i Oslo")]
-        [InlineData("Moskva", "Sne", 2.3, "Regn", 2.8, "Kælke tid i Moskva i Sne vejr med 2,3 grader. I går var det Regn med 2,8 grader")]
-        [InlineData("Stockholm", "Andet", 11, "Skyet", 1.2, "Det samme Andet vejr i Stockholm med 11 grader. I går var det Skyet med 1,2 grader")]
+        [InlineData("Moskva", "Klart vejr", 2.3, "Sne", -2.8, "Kælk og Sne vejr med -2,8 er forbi. Nu bliver det flot Klart vejr med 2,3 i Moskva")]
         [InlineData("Oslo", "Andet", 11, "Andet", 1.2, "Ikke så meget at sige. Andet vejr igår og igen idag med temperatur på 11 i Oslo")]
-        [InlineData("Dubai", "Klart vejr", 30.6, "Andet", 29.8, "Juhuu Klart vejr i Dubai med 30,6 grader. I går var det Andet kedeligt med 29,8 grader")]
+        [InlineData("Dubai", "Klart vejr", 30.6, "Andet", 29.8, "Flot Klart vejr i Dubai med 30,6 grader. I går var det Andet med 29,8 grader")]
         [InlineData("Andeby", "Fjolle vejr", 0, "Andet fjollede vejr", 0, "Ukendt vejr type i Andeby er Fjolle vejr og der er 0 grader. I går var det Andet fjollede vejr og 0 grader")]
         public void Test_weather_message_based_on_input(String town, String todaysConditions, double todaysTemperature,
             String yesterdayCondition, double yesterdayTemperature, String expected)
--- Vejrudsigten/Services/WeatherForecast.cs
+++ Vejrudsigten/Services/WeatherForecast.cs
@@ -24,43 +24,62 @@ namespace Vejrudsigten.Services
             var todayInfo = await service.GetTodaysWeather(key, town);
             var yesterdayInfo = await service.GetYesterdaysWeather(key, town);
 
-            return GetWeatherMessageBasedOnInput(town, 
+            return GetWeatherMessageBasedOnInput(town,
                 todayInfo.Conditions, todayInfo.Temperature, yesterdayInfo.Conditions, yesterdayInfo.Temperature);
         }
 
-        public static String GetWeatherMessageBasedOnInput(String town, String todaysConditions, double todaysTemperature, 
+        public static String GetWeatherMessageBasedOnInput(String town, String todaysConditions,
+            double todaysTemperature,
             String yesterdayCondition, double yesterdayTemperature)
         {
             String result = "";
-            
+
+            if (todaysTemperature < 0 && todaysConditions.Contains(sne))
+            {
+                result = "Det bliver koldt med {0} grader og der kommer {1} vejr i {2}. I går var det {3} og {4} grader";
+                return String.Format(result, todaysTemperature, todaysConditions, town, yesterdayCondition,
+                    yesterdayTemperature);
+            }
+
             if (todaysConditions.Contains(klartVejr) && yesterdayCondition.Contains(klartVejr))
             {
                 result = "{0} igen i {1} med {2} grader idag og i går {3} grader";
                 return String.Format(result, todaysConditions, town, todaysTemperature, yesterdayTemperature);
             }
 
-            if (todaysConditions.Contains(klartVejr))
+            if (todaysConditions.Contains(klartVejr) && yesterdayCondition.Contains(sne))
             {
-                result = "Juhuu {0} i {1} med 30,6 grader. I går var det {2} kedeligt med {3} grader";
-                return String.Format(result, todaysConditions, town, yesterdayCondition, yesterdayTemperature);
+                result = "Kælk og {0} vejr med {1} er forbi. Nu bliver det flot {2} med {3} i {4}";
+                return String.Format(result, yesterdayCondition, yesterdayTemperature, todaysConditions,
+                    todaysTemperature, town);
             }
 
-            if (todaysConditions.Contains(sne))
+            if (todaysConditions.Contains(klartVejr))
             {
-                result = "Kælke tid i {0} i {1} vejr med {2} grader. I går var det {3} med {4} grader";
-                return String.Format(result, town, todaysConditions, todaysTemperature, yesterdayCondition, yesterdayTemperature);
+                result = "Flot {0} i {1} med {2} grader. I går var det {3} med {4} grader";
+                return String.Format(result, todaysConditions, town, todaysTemperature, yesterdayCondition,
+                    yesterdayTemperature);
             }
 
             if (todaysConditions.Contains(regn))
             {
-                result = "Vejret i " + town + " er {0} og der er {1} grader. I går var det {2} og {3} grader";
-                return String.Format(result, todaysConditions, todaysTemperature, yesterdayCondition, yesterdayTemperature);
+                result = "Nu kommer der {0} i {1} med {2} grader idag og i går var det {3} med {4} grader";
+                return String.Format(result, todaysConditions, town, todaysTemperature, yesterdayCondition,
+                    yesterdayTemperature);
+            }
+
+            if (todaysConditions.Contains(skyet) && yesterdayCondition.Contains(sne))
+            {
+                result = "Slut med {0} vejr og {1} grader. Nu bliver det {2} og {3} grader i {4}";
+                return String.Format(result, yesterdayCondition, yesterdayTemperature, todaysConditions,
+                    todaysTemperature, town);
             }
 
             if (todaysConditions.Contains(skyet))
             {
-                result = "Slut med {0} vejr og {1} grader. Nu bliver det {2} og {3} grader i {4}";
-                return String.Format(result, yesterdayCondition, yesterdayTemperature, todaysConditions, todaysTemperature, town);
+                result = "{0} vejr i {1} med {2} grader. I går var det {3} og {4} grader";
+                return String.Format(result, todaysConditions, town, todaysTemperature, yesterdayCondition,
+                    yesterdayTemperature);
             }
 
             if (todaysConditions.Contains(andet) && yesterdayCondition.Contains(andet))
@@ -72,11 +91,14 @@ namespace Vejrudsigten.Services
             if (todaysConditions.Contains(andet))
             {
                 result = "Det samme {0} vejr i {1} med {2} grader. I går var det {3} med {4} grader";
-                return String.Format(result, todaysConditions, town, todaysTemperature, yesterdayCondition, yesterdayTemperature);
+                return String.Format(result, todaysConditions, town, todaysTemperature, yesterdayCondition,
+                    yesterdayTemperature);
             }
 
+
             result = "Ukendt vejr type i {0} er {1} og der er {2} grader. I går var det {3} og {4} grader";
-            return String.Format(result, town, todaysConditions, todaysTemperature, yesterdayCondition, yesterdayTemperature);
+            return String.Format(result, town, todaysConditions, todaysTemperature, yesterdayCondition,
+                yesterdayTemperature);
         }
     }
 }
